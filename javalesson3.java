49bafcc712991cd737e20efdee777a6287ecced7
Margekoorep <165931499+Margekoorep@users.noreply.github.com>
2024-04-03 21:45:09 +0300

    Create javalesson3.java
    "if" statements

---
+++ javalesson3.java
@@ -0,0 +1,60 @@
+public class Main {
+public static void main(String[] args) {
+  System.out.println("Hello world!");
+
+  //if eggs cost <3€ then buy them
+
+  double eggPrice = 2.50;
+  double money = 2.50;
+
+  if(eggPrice >= money){
+
+    System.out.println("Go Work");
+  } else{
+
+    System.out.println("buy the eggs!");
+
+  }
+  System.out.println("This code is going to show always");
+
+  String userName = "Daiga";
+
+
+  if (userName != "Elchin") {
+    System.out.println("You can come to party");
+  } else {
+    System.out.println("Go home!");
+
+
+    TEAMWORK 
+
+    public class teamwork3 {
+        public static void main (String[] args){
+            var eggsPrice = 2.50;
+            var milkPrice = 7.50;
+            var money = 6.14;
+            var isTheLightOn = false;
+            var name1 = "Anna"; //twin sisters
+
+            if (isTheLightOn != false){
+                System.out.println("Switch off the light!");
+            } else {
+                System.out.println("Switch on the light!");
+            }
+
+
+            if (name1 != "Anna"){
+                System.out.println("You are not my sister!!");
+            } else {
+                System.out.println("Hi Anna!");
+            }
+
+
+            if (eggsPrice<money && milkPrice<money){
+                System.out.println("Buy groceries");
+
+            }else {
+                System.out.println("Go home");
+            }
+        }
+    }
