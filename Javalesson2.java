3c26172e2309224d03e25ac431c3455702069348
Margekoorep <165931499+Margekoorep@users.noreply.github.com>
2024-04-03 21:42:51 +0300

    Create Javalesson2.java
    Data types

---
+++ Javalesson2.java
@@ -0,0 +1,33 @@
+
+public class Main {
+  public static void main(String[] args) {
+
+
+    // This is a string
+    var text = "This is a random text";
+    text = "this is another text";
+    text = "March";
+
+    // boolean - lightswitch
+    var isDisabled = false;
+    isDisabled = true;
+
+    var number = 5;
+    number = number + 7;
+
+    //float,character - f lõpus on vajalik
+    var interestingNumber = 100.5f;
+
+    var mixedInterestingNumber = interestingNumber + number; 
+    // number = mixedInterestingNumber -2; // + - * /
+
+    System.out.println(text);
+    System.out.println(isDisabled);
+    System.out.println(interestingNumber);
+    System.out.println(mixedInterestingNumber);
+
+
+  }
+
+
+}
