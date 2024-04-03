a9daec87c6725d7e9c25ba118eede8c9d0039ccf
Margekoorep <165931499+Margekoorep@users.noreply.github.com>
2024-04-03 21:44:01 +0300

    Update Javalesson2.java

--- Javalesson2.java
+++ Javalesson2.java
@@ -28,6 +28,41 @@ public class Main {
 
 
   }
+}
+ // groupwork
+public class Main {
+  public static void main(String[] args) {
+    var name = "Anna"; // string
+    name = "Laura"; // string
+    var age = 25; //integer
+    var student = true; //boolean
+    var yearsStudied = 1;
+    var yearsToStudy = 2;
+    var totalStuding = yearsStudied + yearsToStudy;
+    var bankAccountMoney = 15.23f; //float
+    var savingsAccountMoney = 23.45f; //float
+    var totalMoney = bankAccountMoney + savingsAccountMoney;
+
+    var examMark = "A"; //character
+    var pet = "cat"; //string
+    var petName = "Amy The Gray"; //string
 
 
+    System.out.println(name);
+    System.out.println(name + " is " + age + " years old");
+    System.out.println("Is "+ name + " studying? ");
+    System.out.println(student);
+    System.out.println("How much money is in " + name + "'s bank account? ");
+    System.out.println(bankAccountMoney);
+    System.out.println(name + " has " + savingsAccountMoney + " in her savings");  
+      System.out.println(name +" has " + totalMoney + " in her bank account and savings");
+    System.out.println("What is " + name + "'s exam mark? " + name+ " got an " +examMark);
+    System.out.println("What is "+ name +"'s pet? It is a " +pet+" named "+ petName);
+
+    student = false; // kuna see tuleb hiljem, siis muutub nüüd konsoolis "student false", nii kasutad boolean-i.
+    System.out.println("Is "+ name + " still studying? " + student);
+
+    System.out.println(name + " has studied for " + yearsStudied + " years, in total she needs to study for " + totalStuding + " years"); 
+
+  }  
 }
