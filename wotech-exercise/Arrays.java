9b7644512565f21ded643d0509ae4bf3306ad722
Margekoorep <165931499+Margekoorep@users.noreply.github.com>
2024-04-18 20:23:41 +0300

    Update Arrays.java

--- wotech-exercise/Arrays.java
+++ wotech-exercise/Arrays.java
@@ -94,7 +94,7 @@ public class Main {
 }
 
 
-// a program that calculates the sum and average of elements in an array of integers? 
+// a program that calculates the sum and average of elements in an array of integers? And checks if there are negative numbers in array. 
 
 import java.util.Scanner; 
 public class Main {
@@ -114,6 +114,105 @@ public class Main {
     System.out.println("The sum of array is: " + sumOfArray);
     var averageOfArray = (double) sumOfArray / arr.length;
     System.out.println("The average of array is: " + averageOfArray);
+
+    boolean foundNegative = false;
+    for (int i = 0; i < arr.length; i++){
+      if (arr[i] < 0 ) {
+        foundNegative = true;
+        break;
+      }
+    }
+
+    if (foundNegative) {
+      System.out.print("The negative numbers are: ");
+    for (int i = 0; i < arr.length; i++){
+      if (arr[i] < 0 ){
+        System.out.println(arr[i]);
+      }
+    }
+    } else {
+      System.out.println("There are no negative numbers in the array.");
+      }
+  
+    
      scanner.close();
   }
 }
+ 
+
+// 
+
+/*Fill the party list with people you would like to invite to the party.
+Check whether or not "Anna" is in the array.
+Check whether or not "Maris" is in the array.
+["Oskars", "Anna", "Andris"]
+Result: 
+"Anna is in the party list"
+"Maris is not in the party list"*/
+
+  import java.util.Scanner; 
+public class Main {
+  public static void main(String[] args) {  
+    String[] arr ={"Oskars", "Anna", "Andris"};
+
+    Boolean annaIsInList = false;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == "Anna") {
+        annaIsInList = true;
+        break;
+      }  
+    }
+      boolean marisIsInList = false;
+      for (int i = 0; i < arr.length; i++){
+        if (arr[i] == "Maris"){
+          marisIsInList = true;
+      }
+  }
+   if (annaIsInList){
+  System.out.println("Anna is in the party list");
+   }else{
+  System.out.println("Anna is not in the party list");
+   }
+  if (marisIsInList){
+    System.out.println("Maris is in the party list");
+  } else{
+    System.out.println("Maris is not in the party list");
+  }
+
+  }
+}
+
+
+ /*
+Given an array of integers, write a Java program to find the maximum sum of two integers in the array.
+
+For example in an array like this:
+[2, 3, 1, 5, 4]
+The maximum sum would be 5 + 4
+
+*/
+
+import java.util.Scanner; 
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int[] arr = new int[5];
+    System.out.print("Enter 5 numbers: ");
+    for(int i = 0; i < arr.length; i++){ 
+      arr[i] = scanner.nextInt();
+    }
+    int maxSum = arr[0] + arr[1];
+    
+    for (int i = 0; i < arr.length; i++){
+      for (int j = i + 1; j < arr.length; j++){
+        int maxSum2 = arr[i] + arr[j];
+        if (maxSum2 > maxSum){
+          maxSum = maxSum2;
+        }  
+      }
+    }
+    System.out.println("The maximum sum is: " + maxSum);
+       scanner.close();
+      }
+    }
