908496dfc5a259f0c3c56081c0c6edcca5145c8b
Margekoorep <165931499+Margekoorep@users.noreply.github.com>
2024-04-24 18:48:19 +0300

    Update methods.java

--- wotech-java/methods.java
+++ wotech-java/methods.java
@@ -1,4 +1,3 @@
-//
 
 public class Main {
   public static void main(String[] args) {
@@ -12,8 +11,14 @@ public class Main {
    
     // Method is when we have a code we want to run multiple times
     // Method == function : can be used both names
+  // {} - shows when function starts and ends and that is what method prints/runs
    public static void printOutABorder(){
      System.out.println("===============");
     
   }
     }
+
+// access_modifier: It specifies the visibility of the method. It can be public, private, protected, or default (no modifier). Usually we use public!
+//return_type: It specifies the type of value that the method returns. It can be any valid Java data type or void if the method does not return any value.
+//method_name: It is the name of the method. It should follow the rules for naming identifiers in Java.
+//parameter_list: It contains zero or more parameters (arguments) that are passed to the method. Each parameter consists of a data type followed by the parameter name.
