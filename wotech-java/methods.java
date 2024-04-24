0704cee5bfc789b283b73d6c50744bcfdfe2341e
Margekoorep <165931499+Margekoorep@users.noreply.github.com>
2024-04-24 18:40:46 +0300

    Update and rename methods to methods.java

---
+++ wotech-java/methods.java
@@ -0,0 +1,19 @@
+//
+
+public class Main {
+  public static void main(String[] args) {
+    printOutABorder();
+      
+    printOutABorder();
+      
+    printOutABorder();
+  }
+      
+   
+    // Method is when we have a code we want to run multiple times
+    // Method == function : can be used both names
+   public static void printOutABorder(){
+     System.out.println("===============");
+    
+  }
+    }
