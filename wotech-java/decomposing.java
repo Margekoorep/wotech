0568cd2f0932794773c9ba6625f682768eb1d75b
Margekoorep <165931499+Margekoorep@users.noreply.github.com>
2024-05-08 19:31:41 +0300

    Update decomposing.java

--- wotech-java/decomposing.java
+++ wotech-java/decomposing.java
@@ -25,3 +25,92 @@ public class Main {
     
   }
 }
+
+// Using function to make code better and easier to read: 
+
+public class Main {
+  public static void main(String[] args) {
+    int [] arr = {8, 7, 5 ,3, 2, 1}; // current race result
+    int number = 0; // our result
+    int place = getThePlace (arr, number);
+    System.out.println("Our place is: " + place);
+    // one should use scanner and print out line in Main method
+    }
+    
+  // using fuction to make code shorter and easier to read
+  public static int getThePlace(int [] arr, int number){ 
+    // returns an integer
+    for(int i = 0; i < arr.length; i++){
+      if(arr[i] < number){
+        return i + 1;
+      }
+  }
+    return arr.length + 1;
+  }
+}
+
+_______________________________________________________________________________
+
+  /*Detect prime numbers:
+
+Prime number divides only itself or one for exmple 7. 
+
+Figure out algorythm that cheks if number is prime or not. 
+
+6?
+6/1=6
+6/2=3
+6/3=2...
+
+5?
+5/1=5
+5/2=2.5 
+5/3=1.6...
+In order to find out if a number is prime we neet to didvde with all numbers untill the number. 7 beed 1 to 7.
+
+1. Go through the nubers fron 2 to number -1
+2. Check weather or not it is dividabel (number % (7/ it is modulus) i == 0)
+
+3. if the 2nd point is true then it is not prime
+4. if the 2nd is false it is a prime*/
+
+public class Main {
+  public static void main(String[] args) {
+  boolean isAprimeNumber = isPrime (5);
+    System.out.println(isAprimeNumber);
+   
+  }
+  public static boolean isPrime(int number){
+    for(int i = 2; i < number; i++){
+      if (number % i == 0){
+        return false;
+      }
+    }
+    return true;
+  }
+  
+}
+
+// numbers from 0-100
+public class Main {
+  public static void main(String[] args) {
+ 
+
+    for(int i = 0; i <100; i++){
+      boolean isAPrimeNumber = isPrime(i);
+      System.out.println(i + " is a prime number " + isAPrimeNumber);
+    }
+   
+  }
+  public static boolean isPrime(int number){
+    for(int i = 2; i < number; i++){
+      if (number % i == 0){
+        return false;
+      }
+    }
+    return true;
+  }
+  
+}
+
+  
