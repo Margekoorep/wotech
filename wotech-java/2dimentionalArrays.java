6615f513745c90ac61ae2491295c521df4b12507
Margekoorep <165931499+Margekoorep@users.noreply.github.com>
2024-05-09 20:13:01 +0300

    Update 2dimentionalArrays.java

--- wotech-java/2dimentionalArrays.java
+++ wotech-java/2dimentionalArrays.java
@@ -18,9 +18,81 @@ public class Main {
     }
     for (int i= 0; i< array.length; i++){
       for (int j= 0; j< array[i].length; j++){
-      System.out.println(array[i][j]);
+      System.out.print(array[i][j]);
     }
     }
       }
   }
 
+// Another way 
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] array = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        for(int i = 0; i < array.length; i++){ 
+            //array[0] = {1, 2, 3}
+            //array[0].length = 3
+            int[] row = array[i]; // {1, 2, 3} OR {4, 5, 6} OR, {7, 8, 9}
+            for(int j = 0; j < row.length; j++){
+                System.out.print(row[j]); //PROCESSING ROWS HERE
+            }
+            System.out.println();       
+        }  
+    }
+}
+
+_______________________________________________________________________________________
+
+  
+public class Main {
+  public static void main(String[] args) {
+  
+  int[][] array = new int [5][5];
+    
+    for (int i= 0; i< array.length; i++){
+      int[] row= array [i];
+      for (int j= 0; j< row.length; j++){
+          row[j] = j;
+    }
+    }
+    for(int i = 0; i < array. length; i++){
+      for(int j = 0; j < array[i].length; j++){
+        System.out.print(array[i][j] + "|");
+      }
+    System.out.println();
+    System.out.println("----------");
+    }
+      }
+  }
+
+_______________________________________________________________________________
+  
+public class Main {
+  public static void main(String[] args) {
+  
+    int[][] array = new int [5][5];
+      
+    for (int i= 0; i< array.length; i++){
+        int[] row= array [i];
+        for (int j= 0; j< row.length; j++){
+            row[j] = i*j;
+        }
+    }
+    for(int i = 0; i < array. length; i++){
+        for(int j = 0; j < array[i].length; j++){
+          if(array[i][j]<10){
+            System.out.print(array[i][j] + "  ");
+          }else{
+            System.out.print(array[i][j] + " ");
+          }
+        }
+      System.out.println();
+    }
+  }
+  
+}
