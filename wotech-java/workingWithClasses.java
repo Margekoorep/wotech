f4a233cc0bbc9ad109011ad596c0192a711dd384
Margekoorep <165931499+Margekoorep@users.noreply.github.com>
2024-06-05 18:21:29 +0300

    Create workingWithClasses.java

---
+++ wotech-java/workingWithClasses.java
@@ -0,0 +1,3 @@
+Main.java --> // getting user input, sending this input to tsakmanager, show information to the user
+Taskmanager.java -->// add tasks to the list, mark them done and get list
+Task.java --> // template for task (name, description, date isItDone)
