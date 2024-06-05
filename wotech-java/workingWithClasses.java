1db51e23bb552cff4cea4bdd5b31642ff9a24d7c
Margekoorep <165931499+Margekoorep@users.noreply.github.com>
2024-06-05 18:50:25 +0300

    Update workingWithClasses.java

--- wotech-java/workingWithClasses.java
+++ wotech-java/workingWithClasses.java
@@ -1,3 +1,15 @@
 Main.java --> // getting user input, sending this input to tsakmanager, show information to the user
 Taskmanager.java -->// add tasks to the list, mark them done and get list
 Task.java --> // template for task (name, description, date isItDone)
+// MVP : Minimal Viable Product
+
+   
+  // be able to add task
+  // have a list of all tasks 
+  // set task done, by Task name
+  
+  // BONUS:
+  // list of undone tasks - filter
+  //list of done tasks- filter
+  // get a randome quote to stop procrastinating
+
